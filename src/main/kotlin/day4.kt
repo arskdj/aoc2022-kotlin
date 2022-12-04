d4c2126cad02cfc31044cb650ce83e94088c2618
arskdj <68353557+arskdj@users.noreply.github.com>
2022-12-04 12:24:53 +0200

    format code

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -1,27 +1,23 @@
 import java.io.File
 
 fun main() {
-    val pairs = File("src/main/resources/4.input")
-        .readLines()
-        .map{ it
-            .split(',')
-            .map { it
-                .split('-')
-                .map { it.toInt() }
-            }
+    val pairs = File("src/main/resources/4.input").readLines().map {
+        it.split(',').map {
+            it.split('-').map { it.toInt() }
         }
+    }
 
     val part1 = pairs.count { (a, b) -> a contains b || b contains a }
-    val part2 = pairs.count { (a, b) -> a overlaps  b || b overlaps  a }
+    val part2 = pairs.count { (a, b) -> a overlaps b || a contains b }
 
     println(part1)
     println(part2)
 }
 
-infix fun List<Int>.contains (range:List<Int>): Boolean{
+infix fun List<Int>.contains(range: List<Int>): Boolean {
     return this[0] <= range[0] && this[1] >= range[1]
 }
 
-infix fun List<Int>.overlaps (range:List<Int>): Boolean{
-    return this[0] in range[0]..range[1] || this[1] in range[0] .. range[1]
+infix fun List<Int>.overlaps(range: List<Int>): Boolean {
+    return this[0] in range[0]..range[1] || this[1] in range[0]..range[1]
 }
